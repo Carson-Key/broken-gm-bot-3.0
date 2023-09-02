b3c5fde68282c3b14e50caf0bbfe20dc6acfa0d5
Carson-Key <keyc@oregonstate.edu>
2023-09-01 19:33:57 -0700

    Add: Lambda helpers

---
+++ ec2/Broken-GM-Bot-Characters/helpers/aws/lambda.js
@@ -0,0 +1,19 @@
+import AWS from 'aws-sdk'
+
+export const invokeLambda = async (FunctionName, Payload) => {
+        AWS.config.region = 'us-west-2';
+
+		var lambda = new AWS.Lambda();
+		var params = {
+			FunctionName,
+			InvocationType: 'RequestResponse',
+			LogType: 'Tail',
+			Payload: JSON.stringify(Payload)
+		};
+
+		const data = await lambda.invoke(params).promise()
+        const lambdaResponse = JSON.parse(data.Payload)
+        const lambdaResponseBody = JSON.parse(lambdaResponse.body)
+
+        return lambdaResponseBody
+}
